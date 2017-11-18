5df14668d85ac2e0b70fe3202e79c2af50808e51
Yanz <jusyanzzz27@gmail.com>
2017-11-18 01:22:07 +0000

    Lab7

--- world.js
+++ world.js
@@ -1,26 +1,27 @@
 
+/*global $ */
 $(document).ready(function(){
-    $('#lookup').click(function(){ 
-        $.ajax ({
-            type: 'GET',
-            url: 'world.php',
-            data: {country: $("#country").val()},
-            success: function(data){
-                $("#result").html(data);
-            }
-        });
-    
-    });
-    
-    $('#Abutton').click(function(){ 
-        $.ajax ({
-            type: 'GET',
-            url: 'world.php',
-            data: {country: 'all=true'},
-            success: function(data){
-                $('#result').html(data);
-            }
-        });
-    
-    });
+ $('#lookup').click(function(){
+  if($('input[type="checkbox"]').prop("checked") == true){
+   $name= $('input[type="checkbox"]').attr('name');
+   $value= $('input[type="checkbox"]').attr('value');
+   $.ajax({
+    type: 'GET',
+    url: 'world.php',
+    data: { country: "$name'='$value"},
+    success: function(data){
+     $('#result').html(data);
+    }
+   }); 
+  }else{
+    $.ajax({
+    type: 'GET',
+    url: 'world.php',
+    data: { country: $('#country').val()},
+    success: function(data){
+     $('#result').html(data);
+    }
+   }); 
+  }
+ });
 });
